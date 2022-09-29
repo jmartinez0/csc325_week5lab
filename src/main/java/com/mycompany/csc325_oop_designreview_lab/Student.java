a39563ecb369c5b850de92576f076fa23c9482c3
juan <juan@localhost>
2022-09-29 09:59:32 -0400

    ToDo 02

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,7 +8,12 @@ public class Student extends Human {
 	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
-	
+	public String getAddress() {
+            
+        }
+        public void setAddress(String address) {
+            
+        }
 	// ToDo 3: Add a field for GPA and create setter and getter
 	
 	// ToDo 4: Add comments to your code
