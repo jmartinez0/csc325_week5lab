9068e300f7ce64e3f8150d56f4d3d6355ef43fb9
juan <juan@localhost>
2022-09-29 10:06:56 -0400

    ToDo 04

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -6,26 +6,33 @@ package com.mycompany.csc325_oop_designreview_lab;
  */
 public class Student extends Human {
 	// ToDo 1: Make this class a child of Human
-	
 	// ToDo 2: Fix the resulting errors
-	public String getAddress() {
-            
-        }
-        public void setAddress(String address) {
-            
-        }
-	// ToDo 3: Add a field for GPA and create setter and getter
-	private int GPA;
+        // ToDo 3: Add a field for GPA and create setter and getter
+        // ToDo 4: Add comments to your code
         
+        private int GPA;
+        
+        //Method to return address
+        @Override
+	public String getAddress() {
+            return address;
+        }
+        
+        //Method to set address
+        @Override
+        public void setAddress(String address) {
+            this.address = address;
+        }
+        
+        //Method to return GPA
         public int getGPA() {
             return GPA;
         }
 
+        //Method to set GPA
         public void setGPA(int GPA) {
             this.GPA = GPA;
         }
-
-	// ToDo 4: Add comments to your code
-
+        
         
 }
